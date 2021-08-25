af6b9a0227763207a371da284f9f2464857c513a
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-25 18:55:34 +0100

    Addinng new dependencies.

--- src/main/java/com/dipo/mobileappwebservice/ui/controller/UserController.java
+++ src/main/java/com/dipo/mobileappwebservice/ui/controller/UserController.java
@@ -1,10 +1,28 @@
 package com.dipo.mobileappwebservice.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "Create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "Delete user was called";
+    }
 }
