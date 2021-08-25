05899e8ffaf4949e7df84839f24861d65095b5f1
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-25 09:38:35 +0100

    Initial commit

---
+++ src/main/java/com/dipo/mobileappwebservice/MobileAppWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipo.mobileappwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileAppWebServiceApplication.class, args);
+    }
+
+}
