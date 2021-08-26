8dd6d6874ac054548a20cc4063b728f3298333a0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-26 18:31:13 +0100

    Implementing load user by username..

--- src/main/java/com/dipo/mobileappwebservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/dipo/mobileappwebservice/service/impl/UserServiceImpl.java
@@ -6,10 +6,13 @@ import com.dipo.mobileappwebservice.shared.Utils;
 import com.dipo.mobileappwebservice.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -44,7 +47,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) throw new UsernameNotFoundException(email);
+
+        return  new User(userEntity.getEmail(), userEntity.getEncryptedPassword(),  new ArrayList<>());
     }
 }
