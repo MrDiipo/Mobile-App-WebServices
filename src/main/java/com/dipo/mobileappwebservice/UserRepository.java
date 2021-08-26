50effcd97472fd6bbc590242b4b623c6169b6ce1
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-26 12:04:23 +0100

    Implementing UserRepository class.

---
+++ src/main/java/com/dipo/mobileappwebservice/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dipo.mobileappwebservice;
+
+import com.dipo.mobileappwebservice.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}
