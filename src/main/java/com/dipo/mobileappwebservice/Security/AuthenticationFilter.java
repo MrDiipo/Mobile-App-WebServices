ab9ec991fa53f008010314f98e1988f0b46d88d7
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-27 16:56:08 +0100

    Implementing Authentication Filter.

---
+++ src/main/java/com/dipo/mobileappwebservice/Security/AuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.dipo.mobileappwebservice.Security;
+
+import com.dipo.mobileappwebservice.ui.model.request.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AuthenticationFilter  extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException{
+        try{
+            UserLoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), UserLoginRequestModel.class);
+
+            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken( creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void successfulAuthentication(HttpServletResponse req, HttpServletRequest res,
+                                            FilterChain chain, Authentication auth) throws IOException, ServletException {
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
+        String token = Jwts.builder().setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .compact();
+
+        res.setAttribute(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
+
+}
--- src/main/java/com/dipo/mobileappwebservice/Security/WebSecurity.java
+++ src/main/java/com/dipo/mobileappwebservice/Security/WebSecurity.java
@@ -25,7 +25,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().
                 antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).
-                permitAll().anyRequest().authenticated();
+                permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
      }
 
      @Override
