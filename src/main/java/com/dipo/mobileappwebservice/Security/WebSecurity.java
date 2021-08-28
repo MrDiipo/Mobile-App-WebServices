46c1c3d312f024654ca5ba3d00e3ed29a2f87bf8
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-28 12:54:30 +0100

    Customize user authentication URL.

--- src/main/java/com/dipo/mobileappwebservice/Security/WebSecurity.java
+++ src/main/java/com/dipo/mobileappwebservice/Security/WebSecurity.java
@@ -25,11 +25,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests().
                 antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).
-                permitAll().anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                permitAll().anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
      }
 
      @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+     }
+
+     public AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
      }
 }
