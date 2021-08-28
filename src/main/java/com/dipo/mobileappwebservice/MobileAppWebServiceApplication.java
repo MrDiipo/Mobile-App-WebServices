94fbf64021afd2d1603fd3140d0709b75511ec37
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-28 15:23:26 +0100

    Read from a properties file.

--- src/main/java/com/dipo/mobileappwebservice/MobileAppWebServiceApplication.java
+++ src/main/java/com/dipo/mobileappwebservice/MobileAppWebServiceApplication.java
@@ -1,9 +1,11 @@
 package com.dipo.mobileappwebservice;
 
+import com.dipo.mobileappwebservice.Security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -24,4 +26,9 @@ public class MobileAppWebServiceApplication {
         return new SpringApplicationContext();
     }
 
+
+    @Bean(name = "AppProperties")
+    public AppProperties appProperties(){
+        return new AppProperties();
+    }
 }
--- src/main/java/com/dipo/mobileappwebservice/Security/SecurityConstants.java
+++ src/main/java/com/dipo/mobileappwebservice/Security/SecurityConstants.java
@@ -1,10 +1,16 @@
 package com.dipo.mobileappwebservice.Security;
 
+import com.dipo.mobileappwebservice.SpringApplicationContext;
+
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+
+    public static String getTokenSecret(){
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
