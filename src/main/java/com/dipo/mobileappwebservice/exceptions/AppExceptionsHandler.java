13584cc438b2211be9ffa06e7901ba11c97073b7
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-29 19:35:01 +0100

    Handling a specific exception.

---
+++ src/main/java/com/dipo/mobileappwebservice/exceptions/AppExceptionsHandler.java
@@ -0,0 +1,17 @@
+package com.dipo.mobileappwebservice.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class AppExceptionsHandler {
+
+    @ExceptionHandler(value = {UserServiceException.class})
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+}
