a53a4749d57a93c64e73a3f4cce326ba0c6510c6
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-29 19:41:14 +0100

    Returning a custom error message.

--- src/main/java/com/dipo/mobileappwebservice/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/dipo/mobileappwebservice/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.dipo.mobileappwebservice.exceptions;
 
+import com.dipo.mobileappwebservice.ui.model.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +8,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 
     @ExceptionHandler(value = {UserServiceException.class})
     public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
         return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR );
     }
 }
